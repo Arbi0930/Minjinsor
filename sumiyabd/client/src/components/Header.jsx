a89e473d655e1545fbc4f1d4f00b0d3bef6e9b3a
Arbi0930 <mbsavage0930@gmail.com>
2023-11-04 20:44:12 +0800

    bd

---
+++ sumiyabd/client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1 className='font-bold text-sm'>
+            <span className='text-slate-500'>Web system ba tehnology</span>
+            <span className='text-slate-700'>Biy daalt</span>
+        </h1>
+    </header>
+  )
+}
---
+++ sumiyabd/client/src/pages/Listing.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export const Listing = () => {
+  return (
+    <div>Listing</div>
+  )
+}
